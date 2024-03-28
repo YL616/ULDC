105882aa5e37a0cbe26cdf7fc94e513673a04d02
Yl <142292144+YL616@users.noreply.github.com>
2024-03-28 16:49:19 +0800

    contrastive_loss.py

--- contrastive_loss.py
+++ contrastive_loss.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import math
 
+
 class InstanceLoss(nn.Module):
     def __init__(self, batch_size, temperature, device):
         super(InstanceLoss, self).__init__()
